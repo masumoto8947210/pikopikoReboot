3d6bed40f2c8af81bf9cd72c9d3644458e01f7a9
KaPiBaR-001 <136887614+KaPiBaR-001@users.noreply.github.com>
2024-01-22 15:50:04 +0900

    tp4

--- Assets/Script/Gimmick2.cs
+++ Assets/Script/Gimmick2.cs
@@ -6,6 +6,7 @@ public class Gimmick2 : MonoBehaviour
 {
     public GameObject player;
     public Transform tp_pea;
+    public Transform tp;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,13 @@ public class Gimmick2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(find.instance.Touch && Input.GetKey(KeyCode.Q))
+        if(find.instance.Tp && Input.GetKeyDown(KeyCode.Q))
         {
             player.transform.position = tp_pea.position;
+        }
+        if (find.instance.Tp_pea && Input.GetKeyDown(KeyCode.Q))
+        {
+            player.transform.position = tp.position;
         }
     }
 }
